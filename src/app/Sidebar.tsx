ef7bc9277d01162b004b3c40a727fff66e4d5299
PeterTYLiu <peter.ty.liu@gmail.com>
2026-01-10 23:29:49 -0500

    Group entries in side panel

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -6,6 +6,10 @@ export default async function Sidebar() {
   const entries = await prismaSingleton.entry.findMany({
     orderBy: { date: "desc" },
   });
+  const groupedEntries = Object.groupBy(entries, (entry) =>
+    entry.date.toDateString()
+  );
+  const todayDateString = new Date().toDateString();
   return (
     <div className="p-4" style={{ width: "350px", overflowY: "auto" }}>
       <div className="flex">
@@ -14,22 +18,29 @@ export default async function Sidebar() {
         </form>
       </div>
       <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
-        {entries.map((entry) => {
-          const isToday = entry.date.getDate() === new Date().getDate();
+        {Object.entries(groupedEntries).map(([dateString, entriesInGroup]) => {
+          if (!entriesInGroup) return null; // This makes TS happy
+          const isToday = dateString === todayDateString;
           return (
-            <Link
-              href={"/entry/" + entry.id}
-              key={entry.id}
-              className="hover:bg-overlay p-2"
-            >
-              <h3>
-                {entry.date.toDateString()}
+            <div key={dateString}>
+              <h3 className="text-text-secondary">
+                {dateString}
                 {isToday && <span> (today)</span>}
               </h3>
-              <p id={entry.id} className="line-clamp-2">
-                {entry.text || <i style={{ opacity: 0.4 }}>Empty</i>}
-              </p>
-            </Link>
+              {entriesInGroup.map((entry) => {
+                return (
+                  <Link
+                    href={"/entry/" + entry.id}
+                    key={entry.id}
+                    className="hover:bg-overlay p-2 block"
+                  >
+                    <p id={entry.id} className="line-clamp-2">
+                      {entry.text || <i style={{ opacity: 0.4 }}>Empty</i>}
+                    </p>
+                  </Link>
+                );
+              })}
+            </div>
           );
         })}
       </div>
