f68ab99c39c2dfe8e7e7c95e017dd3face9b1e94
PeterTYLiu <peter.ty.liu@gmail.com>
2026-01-19 22:58:28 -0500

    Added 404 page

---
+++ src/app/entry/[entryId]/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="grid items-center h-dvh text-center">
+      <div>
+        <h2 className="text-4xl">🤔</h2>
+        <p>That entry doesn't exist!</p>
+      </div>
+    </div>
+  );
+}
--- src/app/entry/[entryId]/page.tsx
+++ src/app/entry/[entryId]/page.tsx
@@ -1,5 +1,6 @@
 import Editor from "@/app/entry/[entryId]/Editor";
 import { prismaSingleton } from "@/app/prismaSingleton";
+import { notFound } from "next/navigation";
 
 export default async function Entry({
   params,
@@ -8,9 +9,14 @@ export default async function Entry({
 }) {
   const { entryId } = await params;
 
-  const entry = await prismaSingleton.entry.findUniqueOrThrow({
-    where: { id: entryId },
-  });
+  const entry = await prismaSingleton.entry
+    .findUniqueOrThrow({
+      where: { id: entryId },
+    })
+    .catch((reason) => {
+      console.error(`Error fetching entry ${entryId}: ` + reason);
+      notFound();
+    });
 
   return <Editor entry={entry} />;
 }
